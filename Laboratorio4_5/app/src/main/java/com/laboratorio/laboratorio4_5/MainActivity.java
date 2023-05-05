9fed5d5af7cc9eec371fd1b2300f7cfd25d083cc
JoelAparicio <105009975+JoelAparicio@users.noreply.github.com>
2023-05-05 16:10:56 -0500

    Se hicieron cambios en el laboratorio 4_5

--- Laboratorio4_5/app/src/main/java/com/laboratorio/laboratorio4_5/MainActivity.java
+++ Laboratorio4_5/app/src/main/java/com/laboratorio/laboratorio4_5/MainActivity.java
@@ -4,31 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.Toast;
+
+
 
 
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView img1, img2, img3;
+    private RadioButton rb1, rb2, rb3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        img1 = findViewById(R.id.img1);
-        img2 = findViewById(R.id.img2);
-        img3 = findViewById(R.id.img3);
+        rb1 = findViewById(R.id.rb1);
+        rb2 = findViewById(R.id.rb2);
+        rb3 = findViewById(R.id.rb3);
 
-        img1.setOnClickListener(v -> {
+
+        rb1.setOnClickListener(v -> {
             Toast.makeText(this, "Panda rojo", Toast.LENGTH_SHORT).show();
         });
 
-        img2.setOnClickListener(v -> {
+        rb2.setOnClickListener(v -> {
             Toast.makeText(this, "Aguila imperial blanca", Toast.LENGTH_SHORT).show();
         });
 
-        img3.setOnClickListener(v -> {
+        rb3.setOnClickListener(v -> {
             Toast.makeText(this, "Leopardo de las nieves", Toast.LENGTH_SHORT).show();
         });
 
