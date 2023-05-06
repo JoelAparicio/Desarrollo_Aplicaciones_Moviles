b7edf4e7da619fe1c8860bd5018575dc81596abc
JoelAparicio <105009975+JoelAparicio@users.noreply.github.com>
2023-05-05 23:24:27 -0500

    avances en el proyecto 1

--- Proyecto1/app/src/main/java/com/proyecto/proyecto1/registrarse.java
+++ Proyecto1/app/src/main/java/com/proyecto/proyecto1/registrarse.java
@@ -2,22 +2,26 @@ package com.proyecto.proyecto1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 
-
-public class MainActivity extends AppCompatActivity {
+public class registrarse extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_registrarse);
+
         Intent intent = getIntent();
+
+
+
+
     }
 
-    public void registrarse(View v){
-        Intent intent = new Intent(this, registrarse.class);
+    public void entrar(View v){
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
